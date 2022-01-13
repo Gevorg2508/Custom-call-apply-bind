5b5ed6626e380de9d334768338d94c53ccfd0641
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2022-01-13 14:20:55 +0400

    feat: the last version of this tasks

--- new.js
+++ new.js
@@ -1,19 +1,38 @@
-function User(name){
-     this.name = name;
-    return [1, 2, 3];
-}
+// function User(name){
+//      this.name = name;
+//     return [1, 2, 3];
+// }
 
-function fakeNew(construct, ...rest){
-    const obj = {};
-    obj.__proto__ = construct.prototype;
-    const returnValue = construct.apply(obj, rest);
-    if(typeof returnValue === "object"){
-        return returnValue;
-    }
-    return obj;
-}
-const user = new User("Vrejh");
-const user1 = fakeNew(User, "Vrejh");
+// function fakeNew(construct, ...rest){
+//     const obj = {};
+//     obj.__proto__ = construct.prototype;
+//     const returnValue = construct.apply(obj, rest);
+//     if(typeof returnValue === "object"){
+//         return returnValue;
+//     }
+//     return obj;
+// }
+// const user = new User("Vrejh");
+// const user1 = fakeNew(User, "Vrejh");
 
-console.log(user);
-console.log(user1);
+// console.log(user);
+// console.log(user1);
+
+
+// // function User(name){
+// //     this.name = name;
+// //     return [1, 2, 3, 4];
+// // }
+// // function customNew(context, ...rest){
+// //     const obj = {};
+// //     obj.__proto__ = context.prototype;
+// //     const returnValue = context.apply(obj, rest);
+// //     if(typeof returnValue === "object"){
+// //         return returnValue;
+// //     }
+// //     return obj;
+// // }
+
+// // let original = new User("Gevorg");
+// // let custom = customNew(User, "Gevorg");
+// // console.log(original);
