591e3e09b31f2995afa3f0237283308a9b5033a9
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2022-01-13 13:43:13 +0400

    feat: customApply emplamantation

--- customApply.2.js
+++ customApply.2.js
@@ -13,3 +13,17 @@ name: "Vrejh",
 }
 fn.apply(user, [1, 2, 3]);
 fn.customApply(user, [1, 2, 3]);
+
+
+// Function.prototype.customApply = function customApply(self, rest){
+//     self._fn = this;
+//     return self._fn(rest);
+// }
+// function fn (){
+//     console.log(this.name);
+// }
+// const obj = {
+//     name: "Gevorg",
+// }
+// fn.apply(obj, [1,2,3,4]);
+// fn.customApply(obj, [1,2,3,4]);
